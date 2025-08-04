2f914cabf7d4c8f9011f377187a1cfb6cad26954
Roi Vaknin <vroi4488@gmail.com>
2025-08-04 15:12:04 +0300

    Update App.js to include other components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Board from './Board';
+import Menu from './Menu';
+import WinningScreen from './WinningScreen';
 
 function App() {
   return (
     <div className="App">
       <h1>Tic-Tac-Toe</h1>
+      <Menu />
+      <Board />
+      <WinningScreen />
     </div>
   );
 }
