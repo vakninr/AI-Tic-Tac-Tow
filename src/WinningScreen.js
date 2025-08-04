91e3eddba8acb62df9ead4c8f3c944ceee958066
Roi Vaknin <vroi4488@gmail.com>
2025-08-04 15:11:58 +0300

    Create WinningScreen.js

---
+++ src/WinningScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function WinningScreen() {
+  return (
+    <div className="WinningScreen">
+      <h2>Winning Screen</h2>
+    </div>
+  );
+}
+
+export default WinningScreen;
