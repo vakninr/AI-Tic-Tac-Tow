3391db7e40cbbe681681f4d411f64a421fc40f1f
vakninr <vroi4488@gmail.com>
2025-08-04 23:20:43 +0300

    fixed project structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Board from './Board';
+import Menu from './Menu';
+import WinningScreen from './WinningScreen';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Tic-Tac-Toe</h1>
+      <Menu />
+      <Board />
+      <WinningScreen />
     </div>
   );
 }
